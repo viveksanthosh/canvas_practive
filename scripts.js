6f2e43db344b4bd8c0d44d72f565e76eb465b913
Viveksanthosh <viveksanthosh@holidayiq.com>
2019-05-13 15:19:27 +0530

    drew a rectangle with lines

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 const canvas = document.getElementById('canvas');
 
 const Canvas = canvas.getContext('2d');
-
-Canvas.moveTo(0, 0);
-Canvas.lineTo(1000, 1000);
+Canvas.moveTo(100, 100);
+Canvas.lineTo(100, 200);
+Canvas.lineTo(200, 200);
+Canvas.lineTo(200, 100);
+Canvas.lineTo(100, 100);
 Canvas.stroke();
